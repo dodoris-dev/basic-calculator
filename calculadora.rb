05b427622d49ca67de1e8304aa22bb8003198f00
Doris <91160646+dodoris-dev@users.noreply.github.com>
2022-08-11 15:31:32 -0300

    turns interger into float
    turning interger into float so that the calculator can do operations with fractional numbers (instead of being able to calculate only whole numbers)

--- calculadora.rb
+++ calculadora.rb
@@ -6,26 +6,26 @@ operation = gets.chomp()
     num1 = gets.chomp()
     puts "Now, put another number" 
     num2 = gets.chomp()
-    puts num1.to_i + num2.to_i
+    puts num1.to_f + num2.to_f
   end
   if operation == "subtraction"
     puts "Subtraction? Okay. Please, choose a number"
     num1 = gets.chomp()
     puts "Now, put another number" 
     num2 = gets.chomp()
-    puts num1.to_i - num2.to_i   
+    puts num1.to_f - num2.to_f   
   end
  if operation == "division"
     puts "Division? Okay. Please, choose a number"
     num1 = gets.chomp()
     puts "Now, put another number" 
     num2 = gets.chomp()
-    puts num1.to_i / num2.to_i  
+    puts num1.to_f / num2.to_f 
   end
   if operation == "multiplication"
     puts "Multiplication? Okay. Please, choose a number"
     num1 = gets.chomp()
     puts "Now, put another number" 
     num2 = gets.chomp()
-    puts num1.to_i * num2.to_i  
+    puts num1.to_f * num2.to_f 
   end
