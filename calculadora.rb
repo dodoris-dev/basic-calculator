c08377752acd39cc3d7c2dad3edcd5bfc4d8f549
Doris <91160646+dodoris-dev@users.noreply.github.com>
2022-08-11 15:19:10 -0300

    add multiplication operation

--- calculadora.rb
+++ calculadora.rb
@@ -22,3 +22,10 @@ operation = gets.chomp()
     num2 = gets.chomp()
     puts num1.to_i / num2.to_i  
   end
+  if operation == "multiplication"
+    puts "Multiplication? Okay. Please, choose a number"
+    num1 = gets.chomp()
+    puts "Now, put another number" 
+    num2 = gets.chomp()
+    puts num1.to_i * num2.to_i  
+  end
