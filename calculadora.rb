4dad5b7cee47d6a68e1eec26c5339e91c6379f3e
Doris <91160646+dodoris-dev@users.noreply.github.com>
2022-08-11 14:52:32 -0300

    add rudimentary menu and sum option

---
+++ calculadora.rb
@@ -0,0 +1,10 @@
+puts "Welcome to my little calculator!"
+puts "Now then, tell me what kind of mathematic operation do you want to do?"
+operation = gets.chomp()
+  if operation == "sum" then
+    puts "Sum? Okay. Please, choose a number"
+    num1 = gets.chomp()
+    puts "Now, put another number" 
+    num2 = gets.chomp()
+    puts num1.to_i + num2.to_i
+  end
